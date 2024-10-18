4d6e3452f57bf68769b571d242cce0f5ce2cfaa3
Alvar Arias <alvararias@gmail.com>
2024-10-18 10:29:29 +0200

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Alvar Arias on 2024-10-18.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
